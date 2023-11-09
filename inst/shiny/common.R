68ce45fe7d57a65f9f25ebe7d85072bc91d4b078
simon-smart88 <simon.smart@cantab.net>
2023-11-09 14:55:11 +0000

    move common to single file

---
+++ inst/shiny/common.R
@@ -0,0 +1,25 @@
+common_class <- R6::R6Class(
+  classname = "common",
+  public = list(
+    shape = NULL,
+    agg = NULL,
+    covs = NULL,
+    cov_sum = NULL,
+    prep = NULL,
+    fit = NULL,
+    pred = NULL,
+    map_layers = NULL,
+    poly = NULL,
+    logger = NULL,
+    meta = NULL,
+    countries = readRDS(system.file("ex/countries.rds", package="geodata")),
+    add_map_layer = function(new_names) {
+      for (new_name in new_names){
+        if (!(new_name %in% self$map_layers)){
+          self$map_layers <- c(self$map_layers,new_name)
+          invisible(self)
+        }
+      }
+    }
+  )
+)
