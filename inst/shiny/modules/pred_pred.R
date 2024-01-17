5f88a33768541cac117efbd480b89d80621ff2b7
simon-smart88 <simon.smart@cantab.net>
2024-01-17 17:47:27 +0000

    migrate to disaggregation::predict_model

--- inst/shiny/modules/pred_pred.R
+++ inst/shiny/modules/pred_pred.R
@@ -11,12 +11,15 @@ pred_pred_module_server <- function(id, common) {
 
   observeEvent(input$run, {
     # WARNING ####
-
+    if (is.null(common$fit)){
+      common$logger %>% writeLog(type = "error", "Please fit a model first")
+      return()
+    }
     # FUNCTION CALL ####
-    show_loading_modal("Please wait while the model predictions are being generated")
-    prediction <- predict(common$fit)
-    prediction$mean_prediction$field <- terra::mask(prediction$mean_prediction$field, common$agg)
-    terra::crs(prediction$mean_prediction$field) <- terra::crs(common$covs[[1]])
+    show_loading_modal("Please wait while the model predictions are generated")
+    prediction <- disaggregation::predict_model(common$fit)
+    prediction$field <- terra::mask(prediction$field, common$agg)
+    terra::crs(prediction$field) <- terra::crs(common$covs[[1]])
     close_loading_modal()
     common$logger %>% writeLog('Model predictions are available')
     # LOAD INTO COMMON ####
@@ -50,18 +53,18 @@ pred_pred_module_server <- function(id, common) {
 # }
 
 pred_pred_module_map <- function(map, common) {
-  observeEvent(gargoyle::watch("pred_pred"),{
+  gargoyle::on("pred_pred",{
   req(common$pred)
   common$add_map_layer(c("Field", "Prediction"))
 
   ex <- as.vector(terra::ext(common$shape))
-  pal1 <- colorBin("YlOrRd", domain = terra::values(common$pred$mean_prediction$field), bins = 9, na.color = "#00000000")
-  pal2 <- colorBin("YlOrRd", domain = terra::values(common$pred$mean_prediction$prediction), bins = 9, na.color = "#00000000")
+  pal1 <- colorBin("YlOrRd", domain = terra::values(common$pred$field), bins = 9, na.color = "#00000000")
+  pal2 <- colorBin("YlOrRd", domain = terra::values(common$pred$prediction), bins = 9, na.color = "#00000000")
   map %>%
-    addRasterImage(raster::raster(common$pred$mean_prediction$field), group = "Field", colors = pal1) %>%
-    addLegend(position = "bottomleft", pal = pal1, values = terra::values(common$pred$mean_prediction$field), group = "Field", title = "Field") %>%
-    addRasterImage(raster::raster(common$pred$mean_prediction$prediction), group = "Prediction", colors = pal2) %>%
-    addLegend(position = "bottomright", pal = pal2, values = terra::values(common$pred$mean_prediction$prediction), group = "Prediction", title = "Prediction") %>%
+    addRasterImage(raster::raster(common$pred$field), group = "Field", colors = pal1) %>%
+    addLegend(position = "bottomleft", pal = pal1, values = terra::values(common$pred$field), group = "Field", title = "Field") %>%
+    addRasterImage(raster::raster(common$pred$prediction), group = "Prediction", colors = pal2) %>%
+    addLegend(position = "bottomright", pal = pal2, values = terra::values(common$pred$prediction), group = "Prediction", title = "Prediction") %>%
     fitBounds(lng1 = ex[[1]], lng2 = ex[[2]], lat1 = ex[[3]], lat2 = ex[[4]]) %>%
     addLayersControl(overlayGroups = common$map_layers, options = layersControlOptions(collapsed = FALSE)) %>%
     hideGroup(common$map_layers[2:(length(common$map_layers)-2)]) #hide all but first and last two layers
@@ -71,7 +74,7 @@ pred_pred_module_map <- function(map, common) {
 pred_pred_module_rmd <- function(common) {
   # Variables used in the module's Rmd code
   list(
-    pred_pred_knit = !is.null(common$some_object),
+    pred_pred_knit = common$meta$pred$used,
     var1 = common$meta$setting1,
     var2 = common$meta$setting2
   )
