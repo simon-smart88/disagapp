b2cc04516c6929d5d99a8a05b56a29b94de931cb
simon-smart88 <simon.smart@cantab.net>
2024-02-26 11:11:11 +0000

    update tests

--- tests/testthat/test-cov_bioclim.R
+++ tests/testthat/test-cov_bioclim.R
@@ -1,5 +1,17 @@
+df <- data.frame("area" = c("Triesen", "Schellenberg", "Gamprin", "Triesenberg",
+                            "Eschen", "Ruggell", "Mauren", "Schaan", "Balzers",
+                            "Planken","Vaduz"),
+                 "response" = 1:11)
+
+area_column <- "area"
+resp_column <- "response"
+country_code <- "LIE"
+admin_level <- "ADM1"
+
 test_that("Check cov_bioclim function works as expected", {
-  result <- cov_bioclim("LIE", c("Mean temperature", "Mean diurnal range"))
+  shape <- resp_download(df, area_column, resp_column, country_code, admin_level)
+
+  result <- cov_bioclim("LIE", c("Mean temperature", "Mean diurnal range"), shape)
   expect_is(result, "list")
   expect_is(result[[1]], "SpatRaster")
   expect_equal(length(result), 2)
@@ -12,11 +24,23 @@ test_that("Check cov_bioclim function returns errors as expected", {
 test_that("{shinytest2} recording: e2e_cov_nightlight", {
 
   app <- shinytest2::AppDriver$new(app_dir = system.file("shiny", package = "disagapp"), name = "e2e_cov_nightlight")
+
+  app$set_inputs(tabs = "resp")
+  app$set_inputs(respSel = "resp_download")
+  app$upload_file("resp_download-spread" = "../../lie.csv")
+  #app$upload_file("resp_download-spread" = df_path)
+  app$set_inputs("resp_download-response_column" = resp_column)
+  app$set_inputs("resp_download-area_column" = area_column)
+  app$set_inputs("resp_download-country" = "Liechtenstein")
+  app$set_inputs("resp_download-admin" = "ADM1")
+  app$click("resp_download-run")
+
   app$set_inputs(tabs = "cov")
   app$set_inputs(covSel = "cov_bioclim")
   app$set_inputs("cov_bioclim-country" = "Liechtenstein")
   app$set_inputs("cov_bioclim-variables" = c("Mean temperature", "Mean diurnal range"))
   app$click("cov_bioclim-run")
+
   common <- app$get_value(export = "common")
   expect_is(common$covs[[1]], "SpatRaster")
   expect_equal(length(common$covs), 2)
--- tests/testthat/test-cov_landuse.R
+++ tests/testthat/test-cov_landuse.R
@@ -2,24 +2,34 @@ shp <- list.files(system.file("extdata/shapes", package="disagapp"), pattern = "
 shape <- sf::st_read(shp, quiet = TRUE)
 shape <- shape[shape$Name_1 == "Alaotra Mangoro",]
 
-test_that("Check cov_landuse function works as expected", {
-  result <- cov_landuse(shape, 2019, c("Crops"))
-  expect_is(result, "list")
-  expect_is(result[[1]], "SpatRaster")
-})
+shp2 <- list.files(system.file("extdata/shapes", package="disagapp"), full.names = TRUE)
+
+# test_that("Check cov_landuse function works as expected", {
+#   result <- cov_landuse(shape, 2019, c("Crops"))
+#   expect_is(result, "list")
+#   expect_is(result[[1]], "SpatRaster")
+# })
 
 test_that("{shinytest2} recording: e2e_cov_landuse", {
 
   temp_shape <- tempfile(fileext = ".shp")
   sf::st_write(shape, temp_shape, quiet = TRUE)
-  shpdf <- data.frame(datapath = list.files(path = dirname(temp_shape), pattern = gsub(".shp", "", basename(temp_shape)), full.names = TRUE),
-                      name = list.files(path = dirname(temp_shape), pattern = gsub(".shp", "", basename(temp_shape))))
+  # shpdf <- data.frame(datapath = list.files(path = dirname(temp_shape), pattern = gsub(".shp", "", basename(temp_shape)), full.names = TRUE),
+  #                     name = list.files(path = dirname(temp_shape), pattern = gsub(".shp", "", basename(temp_shape))))
+
+  shpdf <- data.frame(datapath = shp2,
+                      name = basename(shp2))
+
 
   app <- shinytest2::AppDriver$new(app_dir = system.file("shiny", package = "disagapp"), name = "e2e_cov_landuse")
+  app$set_inputs(tabs = "resp")
+  app$set_inputs(respSel = "resp_shape")
   app$upload_file("resp_shape-shape" = shpdf$datapath)
   app$set_inputs("resp_shape-resp_var" = "inc")
   app$click("resp_shape-run")
 
+  app$set_inputs(tabs = "cov")
+  app$set_inputs(covSel = "cov_landuse")
   app$set_inputs("cov_landuse-year" = 2019)
   app$set_inputs("cov_landuse-uses" = c("Crops"))
   app$click("cov_landuse-run")
