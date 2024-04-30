9d64ff57cf901ece26abbe3c10224ffeca313b0e
simon-smart88 <simon.smart@cantab.net>
2024-04-30 17:42:47 +0100

    move country UI to server

--- inst/shiny/modules/pred_transfer.R
+++ inst/shiny/modules/pred_transfer.R
@@ -1,7 +1,7 @@
 pred_transfer_module_ui <- function(id) {
   ns <- shiny::NS(id)
   tagList(
-    selectInput(ns("country"), "Select country", choices = c("", common$countries$NAME), multiple = FALSE),
+    uiOutput(ns("country_out")),
     uiOutput(ns("cov_out")),
     uiOutput(ns("agg_out")),
     actionButton(ns("run"), "Transfer predictions")
@@ -10,6 +10,10 @@ pred_transfer_module_ui <- function(id) {
 
 pred_transfer_module_server <- function(id, common, parent_session) {
   moduleServer(id, function(input, output, session) {
+
+    output$country_out <- renderUI({
+      selectInput(session$ns("country"), "Select country", choices = c("", common$countries$NAME), multiple = FALSE)
+    })
 
     output$cov_out <- renderUI({
       if (!is.null(common$meta$cov_upload$used)){
