3b8793ba9fa9b99dcb784fd86392b11b2511738e
simon-smart88 <simon.smart@cantab.net>
2025-07-08 10:30:10 +0100

    skip on windows

--- tests/testthat/test-agg_worldpop.R
+++ tests/testthat/test-agg_worldpop.R
@@ -5,6 +5,8 @@ lie_che_shape <- resp_download(mdf, area_column, resp_column, country_code, admi
 
 test_that("Check agg_worldpop function works as expected for single country", {
 
+  skip_on_os("windows")
+
   result <- agg_worldpop(lie_shape, country_code[1], "Constrained", "100m", 2020)
   expect_is(result, "SpatRaster")
 
@@ -23,7 +25,9 @@ test_that("Check agg_worldpop function works as expected for single country", {
 
 
 test_that("Check agg_worldpop function works as expected for multiple countries", {
-  result <- agg_worldpop(lie_che_shape, country_code, "Unconstrained", "1km", 2020)
+  skip_on_os("windows")
+
+  result <- agg_worldpop(lie_che_shape, country_code, "Constrained", "1km", 2020)
   expect_is(result, "SpatRaster")
   n_cells <- terra::ncell(result)
   expect_gt(n_cells, 50000)
